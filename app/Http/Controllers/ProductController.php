c7cd90d3feaab4b68c826dbad17f7f83efc2d2bd
Bibin John <bibinjohnm123@gmail.com>
2025-06-09 13:59:04 +0530

    ProductController search&date,price fileter commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,10 +13,28 @@ class ProductController extends Controller
         //$products=ProductModel::paginate(5);
 
          $search = $request->input('search');
-            $products = ProductModel::when($search, function ($query, $search) {
+         $price_min=$request->input('price_min');
+         $price_max=$request->input('price_max');
+         $date_from=$request->input('date_from');
+         $date_to=$request->input('date_to');
+            $products = ProductModel::query()
+            ->when($search, function ($query, $search) {
                 return $query->where('productname', 'like', "%{$search}%")
                             ->orWhere('description', 'like', "%{$search}%")
                             ->orWhere('mrp','like',"%{$search}%");
+            })
+
+            ->when($price_min,function($query,$price_min){
+                return $query->where('price','>=',$price_min);
+            })
+            ->when($price_max,function($query,$price_max){
+                return $query->where('price','<=',$price_max);
+            })
+            ->when($date_from,function($query,$date_from){
+                return $query->where('created_at','>=',$date_from);
+            })
+            ->when($date_to,function($query ,$date_to){
+                return $query->where('created_at','<=',$date_to);
             })->paginate(5);
 
             // Append search query to pagination links
